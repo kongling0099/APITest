a4dac5f2c3e6992d50df66034797c3c848770df5
kongling <kongling@smzdm.com>
2020-01-12 22:11:05 +0800

    调整一下

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # 定义一个测试脚本
 from logzero import logger
 from flask import Flask
-app =Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
 
 if __name__ == '__main__':
     app.run()
